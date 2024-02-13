032b41367a26a936218667c3f25cff765aa1fc5c
Francine <franmelopepe2@gmail.com>
2024-02-13 14:16:48 +0100

    navbar props

---
+++ src/Components/Header/NavProps.tsx
@@ -0,0 +1,16 @@
+import "../../Styles/styles.sass";
+
+function NavProps(props: { id: any; link: any; name: any }) {
+  const { id, link, name } = props;
+  return (
+    <nav >
+      <ul >
+        <li id={id}>
+          <a href={link}>{name}</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavProps;
