fc63d4d8329a2a029faee7d06ccd0ffc7a0f10c1
Francine <franmelopepe2@gmail.com>
2024-02-13 19:47:20 +0100

    open menu component

---
+++ src/Icons/OpenMobileMenu.tsx
@@ -0,0 +1,11 @@
+import MobileMenuIcon from "../Assets/Mobile Menu.svg";
+
+function MobileMenu() {
+  return (
+    <div className="nav-button-toggle">
+      <img src={MobileMenuIcon} alt="mobile menu" />
+    </div>
+  );
+}
+
+export default MobileMenu;
