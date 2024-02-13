9270adb7e7ccdb91965b72dde09f9e7838e2625e
Francine <franmelopepe2@gmail.com>
2024-02-13 14:14:00 +0100

    new data content

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,18 +1,39 @@
+import CarousselImage1 from "./Assets/Banner - Image 1.jpg";
+
 export const navHeader = [
+  {
+    name: "About",
+    link: "/about",
+    id: 0,
+  },
+  {
+    name: "Menu",
+    link: "/menu",
+    id: 1,
+  },
+  {
+    name: "Shop",
+    link: "/shop",
+    id: 3,
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+    id: 4,
+  },
+];
+
+export const BannerImage = [
     {
-        name: "About",
-        link: "/about"
+        id: 0,
+        image: CarousselImage1
     },
+    
+];
+
+export const BannerText = [
     {
-        name: "Menu",
-        link: "/menu"
-    },
-    {
-        name: "Shop",
-        link: "/shop"
-    },
-    {
-        name: "Contact",
-        link: "/contact"
+        id: 0,
+        text: "Welcome to California Coffeehouse, where every cup tells a story, and every sip is a journey through the rich tapestry of flavors. Here, we believe that coffee is more than just a pick-me-up, it's a celebration of life's simple pleasures. From the moment you step through our doors, you're not just a customer – you're a cherished guest in our story. The warmth that permeates our space is not just from the carefully crafted beverages but from the genuine smiles and heartfelt conversations that fill the air."
     }
 ]
