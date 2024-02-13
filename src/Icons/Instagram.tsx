17bd83a5bea95359ef5c160278fa4ded95638da7
Francine <franmelopepe2@gmail.com>
2024-02-13 14:18:21 +0100

    icons component

---
+++ src/Icons/Instagram.tsx
@@ -0,0 +1,11 @@
+import IconInstagram from "../Assets/Icon - Instagram.svg";
+
+function Instagram() {
+  return (
+    <div>
+      <img src={IconInstagram} alt="Instagram" />
+    </div>
+  )
+}
+
+export default Instagram
