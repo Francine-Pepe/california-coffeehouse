347e86adca65732065676f037d044af7cd159938
Francine <franmelopepe2@gmail.com>
2024-02-15 11:59:02 +0100

    about us data

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,4 +1,5 @@
 import CarousselImage1 from "./Assets/BannerImage.svg";
+import Frontdoor from "./Assets/about us image.jpg";
 
 export const navHeader = [
   {
@@ -36,4 +37,13 @@ export const BannerText = [
         id: 0,
         text: "Welcome to California Coffeehouse, where every cup tells a story, and every sip is a journey through the rich tapestry of flavors. Here, we believe that coffee is more than just a pick-me-up, it's a celebration of life's simple pleasures. From the moment you step through our doors, you're not just a customer – you're a cherished guest in our story. The warmth that permeates our space is not just from the carefully crafted beverages but from the genuine smiles and heartfelt conversations that fill the air."
     }
+]
+
+export const AboutUsContent = [
+  {
+    image: Frontdoor,
+    alt: "about us",
+    text: "My love affair with coffee began long before California Coffee came to life. The idea of creating a space where coffee isn't just a beverage, but a cherished experience, has been a dream nurtured over countless cups and shared moments. Growing up, I found solace in the ritual of brewing the perfect cup, and that passion has now blossomed into the heart and soul of this coffeehouse.",
+    button: "continue reading"
+  }
 ]
