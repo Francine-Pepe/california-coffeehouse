4a8537757f363866d681db8165571051e5cd1545
Francine <franmelopepe2@gmail.com>
2024-10-22 17:28:59 +0200

    contact component

---
+++ src/Components/Contact.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Contact(props: any) {
+  const { text, contact } = props;
+  return (
+    <section className="contact-container container">
+      <div className="contact-content">
+        <h1>Contact</h1>
+        <div className="box-container">
+          {text.map((data: { text: string }, index: any) => (
+            <div className="contact-box" key={index}>
+              <p>{data.text}</p>
+            </div>
+          ))}
+
+          <div className="contact-box">
+            <p>
+              Do you want to reserve a table? We can be reached by phone or
+              email:
+            </p>
+            {contact.map(
+              (
+                data: { icon: string; alt: string; text: string },
+                index: any
+              ) => (
+                <div className="contact-box-information" key={index}>
+                  <img src={data.icon} alt={data.alt} />
+                  <p>{data.text}</p>
+                </div>
+              )
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
