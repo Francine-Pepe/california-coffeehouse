a28232ca9403cb97b3f11517874236432735f4c9
Francine <franmelopepe2@gmail.com>
2024-10-22 17:29:11 +0200

    footer component

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Contact from "../Contact";
+import { contactInformation, contactText } from "../../Data";
+
+function Footer() {
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  };
+  return (
+    <footer className="footer-container container">
+      <Contact text={contactText} contact={contactInformation} />
+      <div className="footer">
+        <a
+          href="https://www.francinemelopepe.de/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <small>&copy; Copyright {getCurrentYear()} - Francine Melo</small>
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
