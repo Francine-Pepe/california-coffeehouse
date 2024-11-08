5e7b2ea54b3733570d3168775767e8d829ee0738
Francine-Pepe <franmelopepe2@gmail.com>
2024-11-08 14:59:04 +0100

    classname changed

--- src/Components/Props/TextProps.tsx
+++ src/Components/Props/TextProps.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 
 function TextProps(props: any) {
   const { data } = props;
+
   return (
-    <div className="text-container tracking-in-expand-fwd-top">
+    <div className="text-container container">
       {data.map((item: { text: string }, index: any) => (
         <div key={index}>
           <p>{item.text}</p>
